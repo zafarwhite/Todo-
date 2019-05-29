fa5cabe9f6118eeed5604a47c7262cbb6cee5846
Zafar Kudratov <zafarkudratov@gmail.com>
2019-05-29 17:46:56 +0300

    i add commit function of react, show old code alternative for this

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = <h1>Hello world</h1>;
+const el = <h1>Hello World</h1>;
+// const el = React.createElement(
+//     'h1', null, 'Hello World');
+
 
 ReactDOM.render(el, document.getElementById('root'));
