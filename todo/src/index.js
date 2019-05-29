46d628a31dae0c9ef65d82aa9dba36703a925e9f
Zafar Kudratov <zafarkudratov@gmail.com>
2019-05-29 17:57:55 +0300

    the first step

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const el = (
+    <div>
+        <h1> My Todo List</h1>
+        <input placeholder="serch"/>
+        <ul>
+            <li>Drink coffee</li>
+            <li>Make Awesome App</li>
+        </ul>
+    </div>
+)
 
-const el = <h1>Hello World</h1>;
+
+
+
+
+// const el = <h1>Hello World</h1>;
 // const el = React.createElement(
 //     'h1', null, 'Hello World');
 
